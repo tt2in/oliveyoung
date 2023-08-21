21ce96880398ac2b5c65d2e71886e2e3dc655c31
tt2in <kcoolk22@naver.com>
2023-08-21 10:22:31 +0900

    correction

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
 $(function(){
     $('.header').load('../inc/header.html')
     $('.footer').load('../inc/footer.html')
-    $('.today_product').load('../inc/today_product.html')
+    // $('.today_product').load('../inc/today_product.html')
     
     
     $("a[href$='#']").attr("href","#!")
